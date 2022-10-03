3b3eb7f4147a2eab8c7f60fc4df3e31f48a2fed7
merp770g@gmail.com <merp770g@gmail.com>
2022-10-03 21:28:22 +0300

    changes

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_simbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_numbers + password_simbols + password_letters
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    input_password.insert(0, password)
+    pyperclip.copy(password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -23,7 +50,6 @@ def save():
                 input_website.delete(0, END)
                 input_password.delete(0, END)
                 input_password.delete(0, END)
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -49,7 +75,7 @@ password_text.grid(row=3, column=0)
 input_password = Entry(width=18)
 input_password.grid(row=3, column=1)
 
-second_password_text = Button(text="Generate Password")
+second_password_text = Button(text="Generate Password", command=generate_password)
 second_password_text.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=37, command=save)
